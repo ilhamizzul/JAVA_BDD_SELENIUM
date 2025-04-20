71b66401fea8d20a7fa9e60fb7473eb9b97ddb66
ilhamizzul <ilhamizzul@gmail.com>
2025-04-20 22:09:01 +0700

    Add step definitions for login feature
    Introduce `LoginSteps` class implementing Cucumber step definitions for the login feature. This includes methods for navigating to the login page, entering credentials, clicking the login button, and verifying successful navigation to the inventory page.

---
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -0,0 +1,31 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.*;
+import pages.InventoryPage;
+import pages.LoginPage;
+
+public class LoginSteps {
+    LoginPage loginPage = new LoginPage();
+    InventoryPage inventoryPage = new InventoryPage();
+
+    @Given("the user is on the login page")
+    public void navigateToLoginPage() {
+        loginPage.navigateToLoginPage();
+    }
+
+    @When("the user enters valid username {string} and password {string}")
+    public void enterCredentials(String username, String password) {
+        loginPage.fillUsername(username);
+        loginPage.fillPassword(password);
+    }
+
+    @And("clicks the login button")
+    public void clickLogin() {
+        loginPage.clickLoginButton();
+    }
+
+    @Then("the user should be redirected to the Inventory Page")
+    public void validateLoginSuccess() {
+        inventoryPage.verifyPageLoaded();
+    }
+}
